4d312650c2ae55abd2ea725a3fe124993b0f98e5
Sandi Junker <sandi.junker@outlook.com>
2022-09-05 15:45:04 -0500

    Initial Commit

---
+++ CarListing/CarListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListingApp.swift
+//  CarListing
+//
+//  Created by Sandi Junker on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CarListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
