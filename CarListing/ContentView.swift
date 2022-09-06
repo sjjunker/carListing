0e76f6ce6c797bea020a5a05168963e371bbe193
Sandi Junker <sandi.junker@outlook.com>
2022-09-06 04:34:16 -0500

    Begin Project

--- CarListing/ContentView.swift
+++ CarListing/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("My first REAL app. Done by myself.")
             .padding()
     }
 }
